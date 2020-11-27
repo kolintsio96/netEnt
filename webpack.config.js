4255dee5d0b77d0a17ab1e61ca1eb2ba34df16f5
vkolintso <v.kolintso@theomnicore.com>
2020-11-27 16:23:06 +0200

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const webpack = require('webpack');
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              mimetype: 'image/png'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: 'index.html' }],
+    }),
+    new MiniCssExtractPlugin(),
+    new CleanWebpackPlugin()
+  ]
+};
+
+module.exports = config;
